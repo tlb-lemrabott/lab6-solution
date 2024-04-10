311778ffe1c4cd4d483ba07ee8410d529b667d98
Lemrabott <llemrabott52@gmail.com>
2024-04-10 14:19:56 -0500

    submitting part1 of lab6

---
+++ AdsManagementCli/src/main/java/cs489/sdapp/lab6/adsmanagementcli/AdsManagementCliApplication.java
@@ -0,0 +1,13 @@
+package cs489.sdapp.lab6.adsmanagementcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsManagementCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsManagementCliApplication.class, args);
+    }
+
+}
